dceacbc4e04a0f5908d0ee67b008bf16a69ad227
DilinaHirantha <dilina5860717@gmail.com>
2025-05-11 01:01:38 +0530

    add initial code changes

--- test-react-app/src/App.jsx
+++ test-react-app/src/App.jsx
@@ -1,16 +1,21 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const Card = ({title}) => {
+  // useState is a hook that allows you to add state to a functional component
+  const [count, setCount] = useState(0)
+  const [hasLiked, setHasLiked] = useState(false)
+
+  // fetch data from an API or perform some side effect
+  useEffect(() => {
+    console.log(`You have ${hasLiked ? "liked" : "disliked"} the movie ${title}`)
+  },[hasLiked])
+
   return (
-    <div className="card" style={{
-      border: "1px solid #4b5362",
-      padding: "20px",
-      margin: "10px",
-      backgroundColor: "#31363f",
-      borderRadius: "10px",
-      minHeight: "100px",
-    }}>
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount((previousCount)=>previousCount + 1)}>
+      <h2>{title}</h2><br />
+      <h3>{count || null}</h3>
+      <button className="btn" onClick={() => setHasLiked(!hasLiked)}>{hasLiked? "Like" : "DisLike"}</button>
     </div>
   )
 }
